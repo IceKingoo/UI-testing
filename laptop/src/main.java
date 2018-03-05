aeb4fb410c0ee0fc78ec14bb46e7cd660717427f
IceKingoo <erlygin.la@phystech.edu>
2018-03-05 23:11:46 +0300

    kek

---
+++ laptop/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0; i <1; i++) {
+            System.out.println("Hallo, world!");
+
+
+
+
+        }
+    }
+}
